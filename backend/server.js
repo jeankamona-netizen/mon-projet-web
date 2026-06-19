67709858dac6fcd5111411ff642d5f7e129b7818
jeankamona-internaute <jean.kamona@ism.edu.sn>
2026-06-19 23:35:40 +0000

    feat: connexion frontend-backend via fetch, CORS active

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
 // ===== MIDDLEWARE =====
+app.use(cors());
 app.use(express.json());
 
 // ===== IMPORTATION DES ROUTES =====
